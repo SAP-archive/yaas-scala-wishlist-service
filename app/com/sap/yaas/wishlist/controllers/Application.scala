8198b1e20846561a260ee5b6e819e3ad03a4c2d4
Daniel Roth <daniel.roth02@sap.com>
2016-05-31 12:23:37 +0200

    added oauth token client and some basic exceptions

--- app/com/sap/yaas/wishlist/controllers/Application.scala
+++ app/com/sap/yaas/wishlist/controllers/Application.scala
@@ -1,18 +1,30 @@
 package com.sap.yaas.wishlist.controllers
 
+import javax.inject.Inject
+
 import com.sap.yaas.wishlist.model.WishlistItem
 import play.api.libs.json
 import play.api.libs.json.{JsError, JsResult, JsSuccess, Json}
 import play.api.mvc._
+import com.sap.yaas.wishlist.oauth.OAuthTokenService
+import scala.util.{Failure, Success}
+import scala.concurrent.ExecutionContext
+import com.sap.yaas.wishlist.service.RemoteServiceException
 
 
 /**
   * Created by lutzh on 30.05.16.
   */
-class Application extends Controller {
+class Application @Inject() (oauthClient: OAuthTokenService)(implicit context: ExecutionContext) extends Controller {
 
   def list = Action { request =>
-    Ok(Json.toJson(WishlistItem.dummyItem))
+    val token = oauthClient.getToken.onComplete {
+      case Success(value) => 
+        value.access_token
+      case Failure(e) =>
+        throw new RemoteServiceException("Error during token request, please try again.")
+    }
+    Ok(Json.toJson(WishlistItem.dummyItem) + " + " + token)
   }
 
   def update = Action(BodyParsers.parse.json) { request =>
